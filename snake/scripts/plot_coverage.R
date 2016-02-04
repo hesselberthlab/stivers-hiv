19b038c22cce4b15708e97a58dbdaa66544fe111
Jay Hesselberth <jay.hesselberth@gmail.com>
2016-02-04 06:11:34 -0700

    fix weird caps problem

--- snake/scripts/plot_coverage.R
+++ snake/scripts/plot_coverage.R
@@ -1,25 +1,28 @@
 library(dplyr)
 library(tidyr)
 library(readr)
+library(ggplot2)
+library(cowplot)
+library(Cairo)
 
 colnames <- c('chrom','pos','count','sample','strand')
 
-input_tab <- read_tsv('coverage/summary/combined.tab.gz', col_names = colnames)
+input_tab <- read_tsv('snake/results-stivers-hiv/coverage/summary/combined.tab.gz',
+                      col_names = colnames)
 
 # from samtools idxstats <bam> | percent-align.py
-# norm.factors <- tibble(
-#           ~sample, ~num.align, ~norm.factor,
-#           'JS208', 136756192,  0.16227812193,
-#           'JS215', 41396132,   0.11249420598,
-#           'dummy', NA,         NA
-#           )
+norm.factors <- tibble(
+          ~sample, ~num.align, ~norm.factor,
+          'JS208', 136756192,  0.16227812193,
+          'JS215', 41396132,   0.11249420598
+          )
 
 # from samtools idxstats <dechim.bam> | percent-align.py
 # norm.factors <- tibble(
 #           ~sample, ~num.align, ~norm.factor,
 #           'JS208', 21869104,   0.162397653726,
 #           'JS215', 4603896,    0.137028715784,
-#           'dummy', NA,         NA
+#           'dummy', na,         na
 #           )
 
 # with samtools view -c <raw bam>
@@ -27,13 +30,13 @@ input_tab <- read_tsv('coverage/summary/combined.tab.gz', col_names = colnames)
 #           ~sample, ~num.align, ~norm.factor,
 #           'JS208', 22192538, 0.16227812193,
 #           'JS215', 4656825,  0.11249420598,
-#           'dummy', NA,       NA
+#           'dummy', na,       na
 #           )
 
 tab <- input_tab %>%
   mutate(pos = pos + 1) %>%
   left_join(norm.factors) %>%
-  # CPM per 10k / norm.factor
+  # cpm per 10k / norm.factor
   mutate(norm.count = (count / num.align * 10000) * norm.factor) %>%
   select(-num.align, -norm.factor)
 
@@ -56,5 +59,5 @@ combined <- bind_rows(raw_tab, norm_tab)
 
 gp <- ggplot(aes(y = FracU, x = pos), data = combined)
 
-gp + geom_line() + facet_grid(strand ~ type) + xlab('Position') + ylim(0,1)
+gp + geom_line() + facet_grid(strand ~ type) + xlab('Position') + ylim(0,1) + theme_cowplot()
 
